8ad36ee3887ed15ad492d4755acee187b618f2f4
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-05 12:28:25 +0100

    Testing Create with Postman

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -4,10 +4,7 @@ import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,7 +19,7 @@ public class BeerController {
     private final BeerService beerService;
 
     @PostMapping(BEER_PATH)
-    Mono<ResponseEntity<Void>> createNewBeer(BeerDTO beerDTO) {
+    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
         return beerService.saveNewBeer(beerDTO)
                 .map(savedDTO -> ResponseEntity.created(
                                 UriComponentsBuilder.fromPath(BEER_PATH + "/" + savedDTO.getId())
